0e3ec942996b8041e6b6553a68b779ff96746bd4
Anderson Dantas <andersondantas81@gmail.com>
2021-02-10 11:50:50 -0300

    Construção inicial do roteamento das pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Logo from '../../assets/img/Logo.png';
 import "./Menu.css";
-import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
 
 function Menu() {
     return(
         <nav className="Menu">
-            <a href='/'>
-            <img className="Logo" src={Logo} alt='Anderflix logo' />
-            </a>
+            <Link to='/'>
+                <img className="Logo" src={Logo} alt='Anderflix logo' />
+            </Link>
 
-            <ButtonLink className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
